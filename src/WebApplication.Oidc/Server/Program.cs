558df041f72839b3dedd9596974cd584af8f3cff
Michal Moudrý <moudry.michal@live.com>
2024-03-25 15:39:56 +0100

    Added blazor wasm hosted with oidc projects

---
+++ src/WebApplication.Oidc/Server/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder =
+    Microsoft.AspNetCore.Builder.WebApplication.CreateSlimBuilder(args);
+builder.WebHost.ConfigureKestrel(i => i.AddServerHeader = false);
+builder.Services.AddControllers();
+builder.Services.AddHealthChecks();
+
+var app = builder.Build();
+app.UseHealthChecks("/health");
+
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+
+app.MapControllers();
+app.MapFallbackToFile("index.html");
+
+app.Run();
---
+++ src/WebApplication.Oidc/Shared/WeatherForecast.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace WebApplication.Oidc.Shared;
+
+public sealed class WeatherForecast
+{
+    public DateTime Date { get; init; }
+
+    public int TemperatureC { get; init; }
+
+    public string Summary { get; init; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
